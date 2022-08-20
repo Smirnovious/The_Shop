787d2c7b4cff44a35b65536f7c29b95c78d9d20e
Smirnovious <alex0401@gmail.com>
2022-08-20 21:44:08 +0300

    added more styling and buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Product from './components/Product';
+import {Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
    <>
+   <Header />
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+     </Routes>
+     <Footer/>
    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {BrowserRouter as Router,} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>
+  </Router>
 );
 
